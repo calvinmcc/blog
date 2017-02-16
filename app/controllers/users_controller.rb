947516a1420f4d397a5c191e5b7f4648d862e6fc
Calvin McConnell <calvin.mcconnell20@gmail.com>
2017-02-15 16:11:34 -0800

    fix bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
-
+  def new
+    @user = User.new
+  end
 
   def edit
     @user = current_user
+  end
+
+  def  create
+    user_params = params.require(:user).permit([:first_name,
+                                                :last_name,
+                                                :email,
+                                                :password,
+                                                :password_confirmation,
+                                                :admin])
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to home_path, notice: 'Thank you for signing up'
+    else
+      render :new
+    end
   end
 
   def update
